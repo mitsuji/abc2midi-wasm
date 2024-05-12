313b7df08b9d5b46dd39d4294a7e27df54eeff50
mitsuji <tkms@mitsuji.org>
2024-05-12 20:22:16 +0900

    mod: try ccall,cwrap

--- buildw.sh
+++ buildw.sh
@@ -8,4 +8,5 @@ emcc -c -DANSILIBS -O3 deps/abcmidi/queues.c      -obuild/queues.o
 emcc -c -DANSILIBS -O3 deps/abcmidi/parser2.c     -obuild/parser2.o
 emcc -c -DANSILIBS -O3 deps/abcmidi/stresspat.c   -obuild/stresspat.o
 emcc -c -DANSILIBS -O3 deps/abcmidi/music_utils.c -obuild/music_utils.o
+#emcc -obuild/abc2midi.html build/parseabc.o build/store.o build/genmidi.o build/midifile.o build/queues.o build/parser2.o build/stresspat.o build/music_utils.o --shell-file html_template/shell_minimal.html -sEXPORTED_FUNCTIONS=_main -sEXPORTED_RUNTIME_METHODS=ccall,cwrap
 emcc -obuild/abc2midi.html build/parseabc.o build/store.o build/genmidi.o build/midifile.o build/queues.o build/parser2.o build/stresspat.o build/music_utils.o --shell-file html_template/shell_minimal.html -sEXPORTED_RUNTIME_METHODS=callMain
