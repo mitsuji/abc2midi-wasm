46d030d221a66291c44c69f6b28c80e59c619043
mitsuji <tkms@mitsuji.org>
2024-05-13 22:12:14 +0900

    mod: midi2raw.wasm

--- buildw.sh
+++ buildw.sh
@@ -10,3 +10,16 @@ emcc -c -DANSILIBS -O3 deps/abcmidi/stresspat.c   -obuild/stresspat.o
 emcc -c -DANSILIBS -O3 deps/abcmidi/music_utils.c -obuild/music_utils.o
 #emcc -obuild/abc2midi.html build/parseabc.o build/store.o build/genmidi.o build/midifile.o build/queues.o build/parser2.o build/stresspat.o build/music_utils.o --shell-file html_template/shell_minimal.html -sEXPORTED_FUNCTIONS=_main -sEXPORTED_RUNTIME_METHODS=ccall,cwrap
 emcc -obuild/abc2midi.html build/parseabc.o build/store.o build/genmidi.o build/midifile.o build/queues.o build/parser2.o build/stresspat.o build/music_utils.o --shell-file html_template/shell_minimal.html -sEXPORTED_RUNTIME_METHODS=callMain
+
+emcc -c -O3 -Ideps/libtimidity-0.2.7/src deps/libtimidity-0.2.7/src/common.c     -obuild/common.o
+emcc -c -O3 -Ideps/libtimidity-0.2.7/src deps/libtimidity-0.2.7/src/instrum.c    -obuild/instrum.o
+emcc -c -O3 -Ideps/libtimidity-0.2.7/src deps/libtimidity-0.2.7/src/mix.c        -obuild/mix.o  
+emcc -c -O3 -Ideps/libtimidity-0.2.7/src deps/libtimidity-0.2.7/src/output.c     -obuild/output.o
+emcc -c -O3 -Ideps/libtimidity-0.2.7/src deps/libtimidity-0.2.7/src/playmidi.c   -obuild/playmidi.o
+emcc -c -O3 -Ideps/libtimidity-0.2.7/src deps/libtimidity-0.2.7/src/readmidi.c   -obuild/readmidi.o
+emcc -c -O3 -Ideps/libtimidity-0.2.7/src deps/libtimidity-0.2.7/src/resample.c   -obuild/resample.o
+emcc -c -O3 -Ideps/libtimidity-0.2.7/src deps/libtimidity-0.2.7/src/stream.c     -obuild/stream.o
+emcc -c -O3 -Ideps/libtimidity-0.2.7/src deps/libtimidity-0.2.7/src/tables.c     -obuild/tables.o
+emcc -c -O3 -Ideps/libtimidity-0.2.7/src deps/libtimidity-0.2.7/src/timidity.c   -obuild/timidity.o
+emcc -c -O3 -Ideps/libtimidity-0.2.7/src deps/libtimidity-0.2.7/tests/midi2raw.c -obuild/midi2raw.o
+emcc -obuild/midi2raw.html build/common.o build/instrum.o build/mix.o build/output.o build/playmidi.o build/readmidi.o build/resample.o build/stream.o build/tables.o build/timidity.o build/midi2raw.o --shell-file html_template/shell_minimal.html -sEXPORTED_RUNTIME_METHODS=callMain
--- cleanw.sh
+++ cleanw.sh
@@ -12,3 +12,17 @@ rm build/abc2midi.html
 rm build/abc2midi.js
 rm build/abc2midi.wasm
 
+rm build/common.o
+rm build/instrum.o
+rm build/mix.o
+rm build/output.o
+rm build/playmidi.o
+rm build/readmidi.o
+rm build/resample.o
+rm build/stream.o
+rm build/tables.o
+rm build/timidity.o
+rm build/midi2raw.o
+rm build/midi2raw.html
+rm build/midi2raw.js
+rm build/midi2raw.wasm
