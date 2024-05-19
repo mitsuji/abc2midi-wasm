2c4003e47a5ff90a0cd1436f1f247b80bb8ae17c
mitsuji <tkms@mitsuji.org>
2024-05-19 11:59:14 +0900

    mod: refactoring

--- build.sh
+++ build.sh
@@ -1,11 +1,32 @@
 #!/bin/sh
 
-gcc -c -DANSILIBS -O2 deps/abcmidi/parseabc.c    -obuild/parseabc.o
-gcc -c -DANSILIBS -O2 deps/abcmidi/store.c       -obuild/store.o
-gcc -c -DANSILIBS -O2 deps/abcmidi/genmidi.c     -obuild/genmidi.o
-gcc -c -DANSILIBS -O2 deps/abcmidi/midifile.c    -obuild/midifile.o
-gcc -c -DANSILIBS -O2 deps/abcmidi/queues.c      -obuild/queues.o
-gcc -c -DANSILIBS -O2 deps/abcmidi/parser2.c     -obuild/parser2.o
-gcc -c -DANSILIBS -O2 deps/abcmidi/stresspat.c   -obuild/stresspat.o
-gcc -c -DANSILIBS -O2 deps/abcmidi/music_utils.c -obuild/music_utils.o
-gcc -lm -obuild/abc2midi build/parseabc.o build/store.o build/genmidi.o build/midifile.o build/queues.o build/parser2.o build/stresspat.o build/music_utils.o
+emcc -c -DANSILIBS -O3 deps/abcmidi/parseabc.c    -obuild/parseabc.o
+emcc -c -DANSILIBS -O3 deps/abcmidi/store.c       -obuild/store.o
+emcc -c -DANSILIBS -O3 deps/abcmidi/genmidi.c     -obuild/genmidi.o
+emcc -c -DANSILIBS -O3 deps/abcmidi/midifile.c    -obuild/midifile.o
+emcc -c -DANSILIBS -O3 deps/abcmidi/queues.c      -obuild/queues.o
+emcc -c -DANSILIBS -O3 deps/abcmidi/parser2.c     -obuild/parser2.o
+emcc -c -DANSILIBS -O3 deps/abcmidi/stresspat.c   -obuild/stresspat.o
+emcc -c -DANSILIBS -O3 deps/abcmidi/music_utils.c -obuild/music_utils.o
+emcc -obuild/abc2midi.js build/parseabc.o build/store.o build/genmidi.o build/midifile.o build/queues.o build/parser2.o build/stresspat.o build/music_utils.o -sEXPORTED_RUNTIME_METHODS=FS,callMain -sMODULARIZE -sEXPORT_NAME=createAbc2Midi
+cp build/abc2midi.js web/abc2midi.js
+cp build/abc2midi.wasm web/abc2midi.wasm
+
+emcc -c -O3 -Ideps/libtimidity-0.2.7/src deps/libtimidity-0.2.7/src/common.c     -obuild/common.o
+emcc -c -O3 -Ideps/libtimidity-0.2.7/src deps/libtimidity-0.2.7/src/instrum.c    -obuild/instrum.o
+emcc -c -O3 -Ideps/libtimidity-0.2.7/src deps/libtimidity-0.2.7/src/mix.c        -obuild/mix.o  
+emcc -c -O3 -Ideps/libtimidity-0.2.7/src deps/libtimidity-0.2.7/src/output.c     -obuild/output.o
+emcc -c -O3 -Ideps/libtimidity-0.2.7/src deps/libtimidity-0.2.7/src/playmidi.c   -obuild/playmidi.o
+emcc -c -O3 -Ideps/libtimidity-0.2.7/src deps/libtimidity-0.2.7/src/readmidi.c   -obuild/readmidi.o
+emcc -c -O3 -Ideps/libtimidity-0.2.7/src deps/libtimidity-0.2.7/src/resample.c   -obuild/resample.o
+emcc -c -O3 -Ideps/libtimidity-0.2.7/src deps/libtimidity-0.2.7/src/stream.c     -obuild/stream.o
+emcc -c -O3 -Ideps/libtimidity-0.2.7/src deps/libtimidity-0.2.7/src/tables.c     -obuild/tables.o
+emcc -c -O3 -Ideps/libtimidity-0.2.7/src deps/libtimidity-0.2.7/src/timidity.c   -obuild/timidity.o
+emcc -c -O3 -Ideps/libtimidity-0.2.7/src deps/libtimidity-0.2.7/tests/midi2raw.c -obuild/midi2raw.o
+emcc -obuild/midi2raw.js build/common.o build/instrum.o build/mix.o build/output.o build/playmidi.o build/readmidi.o build/resample.o build/stream.o build/tables.o build/timidity.o build/midi2raw.o -sEXPORTED_RUNTIME_METHODS=FS,callMain -sMODULARIZE -sEXPORT_NAME=createMidi2Raw
+cp build/midi2raw.js web/midi2raw.js
+cp build/midi2raw.wasm web/midi2raw.wasm
+
+emcc -O3 -obuild/raw2wav.js deps/raw2wav/raw2wav.c -sEXPORTED_RUNTIME_METHODS=FS,callMain -sMODULARIZE -sEXPORT_NAME=createRaw2Wav
+cp build/raw2wav.js web/raw2wav.js
+cp build/raw2wav.wasm web/raw2wav.wasm
--- clean.sh
+++ clean.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+rm web/abc2midi.js
+rm web/abc2midi.wasm
 rm build/parseabc.o
 rm build/store.o
 rm build/genmidi.o
@@ -8,5 +10,26 @@ rm build/queues.o
 rm build/parser2.o
 rm build/stresspat.o
 rm build/music_utils.o
-rm build/abc2midi
+rm build/abc2midi.js
+rm build/abc2midi.wasm
 
+rm web/midi2raw.js
+rm web/midi2raw.wasm
+rm build/common.o
+rm build/instrum.o
+rm build/mix.o
+rm build/output.o
+rm build/playmidi.o
+rm build/readmidi.o
+rm build/resample.o
+rm build/stream.o
+rm build/tables.o
+rm build/timidity.o
+rm build/midi2raw.o
+rm build/midi2raw.js
+rm build/midi2raw.wasm
+
+rm web/raw2wav.js
+rm web/raw2wav.wasm
+rm build/raw2wav.js
+rm build/raw2wav.wasm
