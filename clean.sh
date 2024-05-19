88feac3d02f4718d0d2094d8609ee17440a8ff9f
mitsuji <tkms@mitsuji.org>
2024-05-19 12:05:18 +0900

    mod: release script

--- clean.sh
+++ clean.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-rm web/abc2midi.js
-rm web/abc2midi.wasm
 rm build/parseabc.o
 rm build/store.o
 rm build/genmidi.o
@@ -13,8 +11,6 @@ rm build/music_utils.o
 rm build/abc2midi.js
 rm build/abc2midi.wasm
 
-rm web/midi2raw.js
-rm web/midi2raw.wasm
 rm build/common.o
 rm build/instrum.o
 rm build/mix.o
@@ -29,7 +25,5 @@ rm build/midi2raw.o
 rm build/midi2raw.js
 rm build/midi2raw.wasm
 
-rm web/raw2wav.js
-rm web/raw2wav.wasm
 rm build/raw2wav.js
 rm build/raw2wav.wasm
---
+++ release.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+cp build/abc2midi.js web/abc2midi.js
+cp build/abc2midi.wasm web/abc2midi.wasm
+
+cp build/midi2raw.js web/midi2raw.js
+cp build/midi2raw.wasm web/midi2raw.wasm
+
+cp build/raw2wav.js web/raw2wav.js
+cp build/raw2wav.wasm web/raw2wav.wasm
