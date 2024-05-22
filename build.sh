2cf06a3eabea3dc12fce6dab51892bc31bc2b12f
mitsuji <tkms@mitsuji.org>
2024-05-22 21:03:23 +0900

    mod: bulid.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+#
+# abc2midi
+#
 emcc -c -DANSILIBS -O3 deps/abcmidi/parseabc.c    -obuild/parseabc.o
 emcc -c -DANSILIBS -O3 deps/abcmidi/store.c       -obuild/store.o
 emcc -c -DANSILIBS -O3 deps/abcmidi/genmidi.c     -obuild/genmidi.o
@@ -11,6 +14,9 @@ emcc -c -DANSILIBS -O3 deps/abcmidi/music_utils.c -obuild/music_utils.o
 emcc -obuild/abc2midi.js build/parseabc.o build/store.o build/genmidi.o build/midifile.o build/queues.o build/parser2.o build/stresspat.o build/music_utils.o -sEXPORTED_RUNTIME_METHODS=FS,callMain -sMODULARIZE -sEXPORT_NAME=createAbc2Midi -sINVOKE_RUN=0
 
 
+#
+# midi2raw
+#
 emcc -c -O3 deps/libtimidity-0.2.7/src/common.c   -obuild/common.o
 emcc -c -O3 deps/libtimidity-0.2.7/src/instrum.c  -obuild/instrum.o
 emcc -c -O3 deps/libtimidity-0.2.7/src/mix.c      -obuild/mix.o
@@ -25,9 +31,15 @@ emcc -c -O3 -Ideps/libtimidity-0.2.7/src deps/libtimidity-0.2.7/tests/midi2raw.c
 emcc -obuild/midi2raw.js build/common.o build/instrum.o build/mix.o build/output.o build/playmidi.o build/readmidi.o build/resample.o build/stream.o build/tables.o build/timidity.o build/midi2raw.o -sEXPORTED_RUNTIME_METHODS=FS,callMain -sMODULARIZE -sEXPORT_NAME=createMidi2Raw -sINVOKE_RUN=0
 
 
+#
+# raw2wav
+#
 emcc -O3 -obuild/raw2wav.js deps/raw2wav/raw2wav.c -sEXPORTED_RUNTIME_METHODS=FS,callMain -sMODULARIZE -sEXPORT_NAME=createRaw2Wav -sINVOKE_RUN=0
 
 
+#
+# abcm2ps
+#
 cd deps/abcm2ps
 ./configure
 cd -
@@ -48,7 +60,9 @@ emcc -c -O3 deps/abcm2ps/syms.c     -obuild/syms.o
 emcc -obuild/abcm2ps.js build/abcm2ps.o build/abcparse.o build/buffer.o build/deco.o build/draw.o build/format.o build/front.o build/glyph.o build/music.o build/parse.o build/subs.o build/svg.o build/syms.o -sEXPORTED_RUNTIME_METHODS=FS,callMain -sMODULARIZE -sEXPORT_NAME=createAbcm2Ps -sINVOKE_RUN=0
 
 
-
+#
+# relese to web directory
+#
 cp build/abc2midi.js web/js/abc2midi.js
 cp build/abc2midi.wasm web/js/abc2midi.wasm
 
